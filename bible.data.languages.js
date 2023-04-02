431c14bad54b6a109280874e9c05e704e4466ef6
John Dyer <johndyer@gmail.com>
2023-04-02 09:26:02 -0500

    Put all languages in a common file

---
+++ bible.data.languages.js
@@ -0,0 +1,84 @@
+bible.addNames(
+    "ar",
+    bible.DEFAULT_BIBLE,
+    ["التكوين","الخروج","اللاويين","العدد","التثنية","يشوع","القضاة","راعوث","صموئيل الأول","صموئيل الثاني","الملوك الأول","الملوك الثاني","أخبار الأيام الأول","أخبار الأيام الثاني","عزرا","نحميا","أستير","أيوب","المزامير","الأمثال","الجامعة","نشيد الأنشاد","أشعياء","أرمياء","مراثي أرمياء","حزقيال","دانيال","هوشع","يوئيل","عاموس","عوبديا","يونان","ميخا","ناحوم","حبقوق","صفنيا","حجي","زكريا","ملاخي","متى","مرقس","لوقا","يوحنا","أعمال الرسل","رومية","1 كورنثوس","2 كورنثوس","غلاطية","أفسس","فيليبي","كولوسي","1 تسالونيكي","2 تسالونيكي","1 تيموثاوس","2 تيموثاوس","تيطس","فيليمون","العبرانيين","يعقوب","1 بطرس","2 بطرس","1 يوحنا","2 يوحنا","3 يوحنا","يهوذا","رؤيا"],
+);
+bible.addNames(
+    "bn",
+    bible.DEFAULT_BIBLE,
+    ['আদিপুস্তক', 'যাত্রাপুস্তক', 'লেবীয়', 'গণনা', 'দ্বিতীয় বিবরণ', 'যিহোশূয়', 'বিচারকর্ত্তৃগণ', 'রূত', '1 শমূয়েল', '2 শমূয়েল', '1 রাজাবলি', '2 রাজাবলি', '1 বংশাবলি', '2 বংশাবলি', 'ইষ্রা', 'নহিমিয়', 'ইষ্টের', 'ইয়োব', 'গীতসংহিতা', 'হিতোপদেশ', 'উপদেশক', 'পরমগীত', 'যিশাইয়', 'যিরমিয়', 'বিলাপ', 'যিহিস্কেল', 'দানিয়েল', 'হোশেয়', 'যোয়েল', 'আমোষ', 'ওবদিয়', 'যোনা', 'মীখা', 'নহূম', 'হবককূক', 'সফনিয়', 'হগয়', 'সখরিয়', 'মালাখি', 'মথি', 'মার্ক', 'লুক', 'যোহন', 'প্রেরিতদের কার্য্য', 'রোমীয়', '1 করিন্থীয়', '2 করিন্থীয়', 'গালাতীয়', 'ইফিষীয়', 'ফিলিপীয়', 'কলসীয়', '1 থিষলনীকীয়', '2 থিষলনীকীয়', '1 তীমথিয়', '2 তীমথিয়', 'তীত', 'ফিলীমন', 'ইব্রীয়', 'যাকোব', '1 পিতর', '2 পিতর', '1 যোহন', '2 যোহন', '3 যোহন', 'যিহূদা', 'প্রকাশিত বাক্য'],
+);
+bible.addNames(
+    "de",
+    bible.DEFAULT_BIBLE,
+    ["Genesis","Exodus","Levitikus","Numeri","Deuteronomium","Josua","Richter","Rut","1.Samuel","2.Samuel","1.Könige","2.Könige","1.Chronik","2.Chronik","Esra","Nehemia","Ester","Hiob","Psalter","Sprüche","Prediger","Hohelied","Jesaja","Jeremia","Klagelieder","Hesekiel","Daniel","Hosea","Joel","Amos","Obadja","Jona","Micha","Nahum","Habakuk","Zefanja","Haggai","Sacharja","Maleachi","Matthäus","Markus","Lukas","Johannes","Apostelgeschichte","Römer","1.Korinther","2.Korinther","Galater","Epheser","Philipper","Kolosser","1.Thessalonicher","2.Thessalonicher","1.Timotheus","2.Timotheus","Titus","Philemon","Hebräer","Jakobus","1.Petrus","2.Petrus","1.Johannes","2.Johannes","3.Johannes","Judas","Offenbarung"],
+);
+bible.addNames(
+    "es",
+    bible.DEFAULT_BIBLE,
+    ["Génesis","Exodo","Levítico","Números","Deuteronomio","Josué","Jueces","Rut","1 Samuel","2 Samuel","1 Reyes","2 Reyes","1 Crónicas","2 Crónicas","Esdras","Nehemías","Ester","Job","Salmos","Proverbios","Eclesiastés","Cantares","Isaías","Jeremías","Lamentaciones","Ezequiel","Daniel","Oseas","Joel","Amós","Abdías","Jonás","Miqueas","Nahúm","Habacuc","Sofonías","Hageo","Zacarías","Malaquías","Mateo","Marcos","Lucas","Juan","Hechos","Romanos","1 Corintios","2 Corintios","Gálatas","Efesios","Filipenses","Colosenses","1 Tesalonicenses","2 Tesalonicenses","1 Timoteo","2 Timoteo","Tito","Filemón","Hebreos","Santiago","1 Pedro","2 Pedro","1 Juan","2 Juan","3 Juan","Judas","Apocalipsis"],
+);
+bible.addNames(
+    "fr",
+    bible.DEFAULT_BIBLE,
+    ["Genèse","Exode","Lévitique","Nombres","Deutéronome","Josué","Juges","Ruth","1 Samuel","2 Samuel","1 Rois","2 Rois","1 Chroniques","2 Chroniques","Esdras","Néhémie","Esther","Job","Psaumes","Proverbes","Ecclésiaste","Cantique","Esaïe","Jérémie","Lamentations","Ezéchiel","Daniel","Osée","Joël","Amos","Abdias","Jonas","Michée","Nahum","Habacuc","Sophonie","Aggée","Zacharie","Malachie","Matthieu","Marc","Luc","Jean","Actes","Romains","1 Corinthiens","2 Corinthiens","Galates","Ephésiens","Philippiens","Colossiens","1 Thessalonicien","2 Thessalonicien","1 Timothée","2 Timothée","Tite","Philemon","Hébreux","Jacques","1 Pierre","2 Pierre","1 Jean","2 Jean","3 Jean","Jude","Apocalypse"],
+);
+bible.addNames(
+    "el",
+    bible.DEFAULT_BIBLE,
+    ['ΓΕΝΕΣΙΣ', 'ΕΞΟΔΟΣ', 'ΛΕΥΙΤΙΚΟΝ', 'ΑΡΙΘΜΟΙ', 'ΔΕΥΤΕΡΟΝΟΜΙΟΝ', 'ΙΗΣΟΥΣ ΝΑΥΗ', 'ΚΡΙΤΑΙ', 'ΡΟΥΘ', 'ΒΑΣΙΛΕΙΩΝ Α΄', 'ΒΑΣΙΛΕΙΩΝ B΄', 'ΒΑΣΙΛΕΙΩΝ Γ΄', 'ΒΑΣΙΛΕΙΩΝ Δ΄', 'ΠΑΡΑΛΕΙΠΟΜΕΝΩΝ Α΄', 'ΠΑΡΑΛΕΙΠΟΜΕΝΩΝ B΄', 'ΕΣΔΡΑΣ', 'ΝΕΕΜΙΑΣ', 'ΕΣΘΗΡ', 'ΙΩΒ', 'ΨΑΛΜΟΙ', 'ΠΑΡΟΙΜΙΑΙ', 'ΕΚΚΛΗΣΙΑΣΤΗΣ', 'ΑΣΜΑ ΑΣΜΑΤΩΝ', 'ΗΣΑΪΑΣ', 'ΙΕΡΕΜΙΑΣ', 'ΘΡΗΝΟΙ ΙΕΡΕΜΙΟΥ', 'ΙΕΖΕΚΙΗΛ ', 'ΔΑΝΙΗΛ', 'ΩΣΗΕ', 'ΙΩΗΛ', 'ΑΜΩΣ', 'ΟΒΔΙΟΥ', 'ΙΩΝΑΣ', 'ΜΙΧΑΙΑΣ', 'ΝΑΟΥΜ', 'ΑΜΒΑΚΟΥΜ', 'ΣΟΦΟΝΙΑΣ ', 'ΑΓΓΑΙΟΣ', 'ΖΑΧΑΡΙΑΣ', 'ΜΑΛΑΧΙΑΣ', 'ΜΑΤΘΑΙΟΝ', 'ΜΑΡΚΟΝ', 'ΛΟΥΚΑΝ', 'ΙΩΑΝΝΗΝ', 'ΠΡΑΞΕΙΣ', 'ΡΩΜΑΙΟΥΣ', 'ΚΟΡΙΝΘΙΟΥΣ Α΄', 'ΚΟΡΙΝΘΙΟΥΣ Β΄', 'ΓΑΛΑΤΑΣ', 'ΕΦΕΣΙΟΥΣ', 'ΦΙΛΙΠΠΗΣΙΟΥΣ', 'ΚΟΛΟΣΣΑΕΙΣ', 'ΘΕΣΣΑΛΟΝΙΚΕΙΣ Α΄', 'ΘΕΣΣΑΛΟΝΙΚΕΙΣ Β΄', 'ΤΙΜΟΘΕΟΝ Α΄', 'ΤΙΜΟΘΕΟΝ Β΄', 'ΤΙΤΟΝ', 'ΦΙΛΗΜΟΝΑ', 'ΕΒΡΑΙΟΥΣ', 'ΙΑΚΩΒΟΥ', 'ΠΕΤΡΟΥ Α΄', 'ΠΕΤΡΟΥ Β΄', 'ΙΩΑΝΝΟΥ Α΄', 'ΙΩΑΝΝΟΥ Β΄', 'ΙΩΑΝΝΟΥ Γ΄', 'ΙΟΥΔΑ', 'ΑΠΟΚΑΛΥΨΙΣ'],
+);
+bible.addNames(
+    "fi",
+    bible.DEFAULT_BIBLE,
+    ["Ensimmäinen Mooseksen kirja","Toinen Mooseksen kirja","Kolmas Mooseksen kirja","Neljäs Mooseksen kirja","Viides Mooseksen kirja","Joosuan kirja","Tuomarien kirja","Ruutin kirja","Ensimmäinen Samuelin kirja","Toinen Samuelin kirja","Ensimmäinen kuninkaiden kirja ","Toinen kuninkaiden kirja ","Ensimmäinen aikakirja ","Toinen aikakirja ","Esran kirja","Nehemian kirja ","Esterin kirja","Jobin kirja ","Psalmien kirja","Sananlaskujen kirja","Saarnaajan kirja","Laulujen laulu","Jesajan kirja ","Jeremian kirja","Valitusvirret ","Hesekielin kirja ","Danielin kirja","Hoosean kirja","Joelin kirja","Aamoksen kirja ","Obadjan kirja ","Joonan kirja","Miikan kirja","Nahumin kirja","Habakukin kirja ","Sefanjan kirja ","Haggain kirja","Sakarjan kirja","Malakian kirja ","Evankeliumi Matteuksen mukaan ","Evankeliumi Markuksen mukaan","Evankeliumi Luukkaan mukaan ","Evankeliumi Johanneksen mukaan ","Apostolien teot ","Kirje roomalaisille","Ensimmäinen kirje korinttilaisille","Toinen kirje korinttilaisille","Kirje galatalaisille ","Kirje efesolaisille ","Kirje filippiläisille ","Kirje kolossalaisille","Ensimmäinen kirje tessalonikalaisille","Toinen kirje tessalonikalaisille","Ensimmäinen kirje Timoteukselle","Toinen kirje Timoteukselle","Kirje Titukselle","Kirje Filemonille","Kirje heprealaisille ","Jaakobin kirje","Ensimmäinen Pietarin kirje","Toinen Pietarin kirje","Ensimmäinen Johanneksen kirje","Toinen Johanneksen kirje","Kolmas Johanneksen kirje ","Juudaksen kirje","Johanneksen ilmestys"],
+    ['1. Moos.','2. Moos.','3. Moos.','4. Moos.','5. Moos.','Joos.','Tuom.','Ruut','1. Sam.','2. Sam.','1. Kun.','2. Kun.','1. Aik.','2. Aik.','Esra','Neh.','Est.','Job','Ps.','Sananl.','Saarn.','Laul. l.','Jes.','Jer.','Valit.','Hes.','Dan.','Hoos.','Joel','Aam.','Ob.','Joona','Miika','Nah.','Hab.','Sef.','Hagg.','Sak.','Mal.','Matt.','Mark.','Luuk.','Joh.','Ap.t.','Room.','1. Kor.','2. Kor.','Gal.','Ef.','Fil.','Kol.','1. Tess.','2. Tess.','1. Tim.','2. Tim.','Tit.','Filem.','Hepr.','Jaak.','1. Piet.','2. Piet.','1. Joh.','2. Joh.','3. Joh.','Juud.','Ilm.']
+);
+bible.addNames(
+    "hi",
+    bible.DEFAULT_BIBLE,
+    ["उत्पत्ति","निर्गमन","लैव्यवस्था","गिनती","व्यवस्थाविवरण","यहोशू","न्यायियों","रूत","1 शमूएल","2 शमूएल","1 राजा","2 राजा","1 इतिहास","2 इतिहास","एज्रा","नहेमायाह","एस्तेर","अय्यूब","भजन संहिता","नीतिवचन","सभोपदेशक","श्रेष्ठगीत","यशायाह","यिर्मयाह","विलापगीत","यहेजकेल","दानिय्येल","होशे","योएल","आमोस","ओबद्दाह","योना","मीका","नहूम","हबक्कूक","सपन्याह","हाग्गै","जकर्याह","मलाकी","मत्ती","मरकुस","लूका","यूहन्ना","प्रेरितों के काम","रोमियो","1 कुरिन्थियों","2 कुरिन्थियों","गलातियों","इफिसियों","फिलिप्पियों","कुलुस्सियों","1 थिस्सलुनीकियों","2 थिस्सलुनीकियों","1 तीमुथियुस","2 तीमुथियुस","तीतुस","फिलेमोन","इब्रानियों","याकूब","1 पतरस","2 पतरस","1 यूहन्ना","2 यूहन्ना","3 यूहन्ना","यहूदा","प्रकाशित वाक्य"],
+);
+bible.addNames(
+    "iw",
+    bible.DEFAULT_BIBLE,
+    ['בראשית', 'שמות', 'ויקרא', 'במדבר', 'דברים', 'יהושע', 'שפטים', 'שמואל א', 'שמואל ב', 'מלכים א', 'מלכים ב', 'ישעיה', 'ירמיה', 'יחזקאל', 'הושע', 'יואל', 'עמוס', 'עבדיה', 'יונה', 'מיכה', 'נחום', 'חבקוק', 'צפניה', 'חגי', 'זכריה', 'מלאכי', 'תהלים', 'משלי', 'איוב', 'שיר השירים', 'רות', 'איכה', 'קהלת', 'אסתר', 'דניאל', 'עזרא', 'נחמיה', 'דברי הימים א', 'דברי הימים ב', 'מתי', 'מרקוס', 'לוקס', 'יוחנן', 'מעשי השליחים', 'אל הרומים', 'הראשונה אל הקורינתים', 'השניה אל הקורינתים', 'אל הגלטים', 'אל האפסים', 'אל הפיליפים', 'אל הקולוסים', 'הראשונה אל התסלוניקים', 'השניה אל התסלוניקים', 'הראשונה אל טימותיאוס', 'השניה אל טימותיאוס', 'אל טיטוס', 'אל פילימון', 'אל העברים', 'אגרת יעקב', 'הראשונה לכיפא', 'השניה לכיפא', 'הראשונה ליוחנן', 'השניה ליוחנן', 'השלישית ליוחנן', 'אגרת יהודה', 'ההתגלות'],
+);
+bible.addNames(
+    "ja",
+    bible.DEFAULT_BIBLE,
+    ['創世記', '出エジプト記', 'レビ記', '民数記', '申命記', 'ヨシュア記', '士師記', 'ルツ記', 'サムエル記上', 'サムエル記下', '列王記上', '列王記下', '歴代誌上', '歴代誌下', 'エズラ記', 'ネヘミヤ記', 'エステル記', 'ヨブ記', '詩篇', '箴言', '伝道の書', '雅歌', 'イザヤ書', 'エレミヤ書', '哀歌', 'エゼキエル書', 'ダニエル書', 'ホセア書', 'ヨエル書', 'アモス書', 'オバデヤ書', 'ヨナ書', 'ミカ書', 'ナホム書', 'ハバクク書', 'ゼパニヤ書', 'ハガイ書', 'ゼカリヤ書', 'マラキ書', 'マタイによる福音書', 'マルコによる福音書', 'ルカによる福音書', 'ヨハネによる福音書', '使徒行伝', 'ローマ人への手紙', 'コリント人への第一の手紙', 'コリント人への第二の手紙', 'ガラテヤ人への手紙', 'エペソ人への手紙', 'ピリピ人への手紙', 'コロサイ人への手紙', 'テサロニケ人への第一の手紙', 'テサロニケ人への第二の手紙', 'テモテへの第一の手紙', 'テモテへの第二の手紙', 'テトスへの手紙', 'ピレモンへの手紙', 'ヘブル人への手紙', 'ヤコブの手紙', 'ペテロの第一の手紙', 'ペテロの第二の手紙', 'ヨハネの第一の手紙', 'ヨハネの第二の手紙', 'ヨハネの第三の手紙', 'ユダの手紙', 'ヨハネの黙示録']
+);
+bible.addNames(
+    "kr",
+    bible.DEFAULT_BIBLE,
+    ['창세기', '출애굽기', '레위기', '민수기', '신명기', '여호수아기', '사사기', '룻기', '사무엘기상', '사무엘기하', '열왕기상', '열왕기하', '역대지상', '역대지하', '에스라기', '느헤미야기', '에스더기', '욥기', '시편', '잠언', '전도서', '아가', '이사야서', '예레미야서', '예레미야 애가', '에스겔서', '다니엘서', '호세아서', '요엘서', '아모스서', '오바댜서', '요나서', '미가서', '나훔서', '하박국서', '스바냐서', '학개서', '스가랴서', '말라기서', '마태복음서', '마가복음서', '누가복음서', '요한복음서', '사도행전', '로마서', '고린도전서', '고린도후서', '갈라디아서', '에베소서', '빌립보서', '골로새서', '데살로니가전서', '데살로니가후서', '디모데전서', '디모데후서', '디도서', '빌레몬서', '히브리서', '야고보서', '베드로전서', '베드로후서', '요한1서', '요한2서', '요한3서', '유다서', '요한계시록']
+);
+bible.addNames(
+    "po",
+    bible.DEFAULT_BIBLE,
+    ['Gênesis', 'Êxodo', 'Levítico', 'Números', 'Deuteronômio', 'Josué', 'Juízes', 'Rute', '1 Samuel', '2 Samuel', '1 Reis', '2 Reis', '1 Crônicas', '2 Crônicas', 'Esdras', 'Neemias', 'Ester', 'Jó', 'Salmo', 'Provérbios', 'Eclesiastes', 'Cântico dos Cânticos', 'Isaías', 'Jeremias', 'Lamentações', 'Ezequiel', 'Daniel', 'Oseias', 'Joel', 'Amós', 'Obadias', 'Jonas', 'Miqueias', 'Naum', 'Habacuque', 'Sofonias', 'Ageu', 'Zacarias', 'Malaquias', 'Mateus', 'Marcos', 'Lucas', 'João', 'Atos', 'Romanos', '1 Coríntios', '2 Coríntios', 'Gálatas', 'Efésios', 'Filipenses', 'Colossenses', '1 Tessalonicenses', '2 Tessalonicenses', '1 Timóteo', '2 Timóteo', 'Tito', 'Filemom', 'Hebreus', 'Tiago', '1 Pedro', '2 Pedro', '1 João', '2 João', '3 João', 'Judas', 'Apocalipse'],
+);
+bible.addNames(
+    "ru",
+    bible.DEFAULT_BIBLE,
+    ['Книга Бытие', 'Книга Исход', 'Книга Левит', 'Книга Числа', 'Книга Второзаконие', 'Иисус Навин', 'Книга Судей', 'Руфь', '1-я Царств', '2-я Царств', '3-я Царств', '4-я Царств', '1-я Паралипоменон', '2-я Паралипоменон', 'Ездра', 'Неемия', 'Есфирь', 'Иов', 'Псалтирь', 'Притчи', 'Екклесиаст', 'Песни Песней', 'Исаия', 'Иеремия', 'Плач Иеремии', 'Иезекииль', 'Даниил', 'Осия', 'Иоиль', 'Амос', 'Авдия', 'Иона', 'Михей', 'Наум', 'Аввакум', 'Софония', 'Аггей', 'Захария', 'Малахия', 'Евангелие от Матфея', 'Марка', 'Луки', 'Иоанна', 'Деяния', 'Римлянам', '1-е Коринфянам', '2-е Коринфянам', 'Послание к Галатам', 'К Ефесянам', 'К Филиппийцам', 'К Колоссянам', '1-е Фессалоникийцам', '2-е Фессалоникийцам', '1-е Тимофею', '2-е Тимофею', 'Послание к Титу', 'Филимону', 'Послание к Евреям •', 'Послание Иакова', '1-e Петра', '2-e Петра', '1-e Иоанна', '2-e Иоанна', '3-e Иоанна', 'Послание Иуды', 'Откровение Иоанна Богослова'],
+);
+bible.addNames(
+    "sv",
+    bible.DEFAULT_BIBLE,
+    ["Ensimmäinen Mooseksen kirja","Toinen Mooseksen kirja","Kolmas Mooseksen kirja","Neljäs Mooseksen kirja","Viides Mooseksen kirja","Joosuan kirja","Tuomarien kirja","Ruutin kirja","Ensimmäinen Samuelin kirja","Toinen Samuelin kirja","Ensimmäinen kuninkaiden kirja ","Toinen kuninkaiden kirja ","Ensimmäinen aikakirja ","Toinen aikakirja ","Esran kirja","Nehemian kirja ","Esterin kirja","Jobin kirja ","Psalmien kirja","Sananlaskujen kirja","Saarnaajan kirja","Laulujen laulu","Jesajan kirja ","Jeremian kirja","Valitusvirret ","Hesekielin kirja ","Danielin kirja","Hoosean kirja","Joelin kirja","Aamoksen kirja ","Obadjan kirja ","Joonan kirja","Miikan kirja","Nahumin kirja","Habakukin kirja ","Sefanjan kirja ","Haggain kirja","Sakarjan kirja","Malakian kirja ","Evankeliumi Matteuksen mukaan ","Evankeliumi Markuksen mukaan","Evankeliumi Luukkaan mukaan ","Evankeliumi Johanneksen mukaan ","Apostolien teot ","Kirje roomalaisille","Ensimmäinen kirje korinttilaisille","Toinen kirje korinttilaisille","Kirje galatalaisille ","Kirje efesolaisille ","Kirje filippiläisille ","Kirje kolossalaisille","Ensimmäinen kirje tessalonikalaisille","Toinen kirje tessalonikalaisille","Ensimmäinen kirje Timoteukselle","Toinen kirje Timoteukselle","Kirje Titukselle","Kirje Filemonille","Kirje heprealaisille ","Jaakobin kirje","Ensimmäinen Pietarin kirje","Toinen Pietarin kirje","Ensimmäinen Johanneksen kirje","Toinen Johanneksen kirje","Kolmas Johanneksen kirje ","Juudaksen kirje","Johanneksen ilmestys"],
+    ["1 Mos","2 Mos","3 Mos","4 Mos","5 Mos","Jos","Dom","Rut","1 Sam","2 Sam","1 Kung","2 Kung","1 Krön","2 Krön","Esr","Neh","Est","Job","Ps","Ords","Pred","Höga v","Jes","Jer","Klag","Hes","Dan","Hos","Joel","Am","Ob","Jon","Mik","Nah","Hab","Sef","Hagg","Sak","Mal","Matt","Mark","Luk","Joh","Apg","Rom","1 Kor","2 Kor","Gal","Ef","Fil","Kol","1 Thess","2 Thess","1 Tim","2 Tim","Tit","Filem","Heb","Jak","1 Pet","2 Pet","1 Joh","2 Joh","3 Joh","Jud","Upp"]
+);
+bible.addNames(
+    "zh-CN",
+    bible.DEFAULT_BIBLE,
+    ["创世记","出埃及记","利未记","民数记","申命记","约书亚记","士师记","路得记","撒母耳记上","撒母耳记下","列王纪上","列王纪下","历代志上","历代志下","以斯拉记","尼希米记","以斯帖记","约伯记","诗篇","箴言","传道书","雅歌","以赛亚书","耶利米书","耶利米哀歌","以西结书","但以理书","何西阿书","约珥书","阿摩司书","俄巴底亚书","约拿书","弥迦书","那鸿书","哈巴谷书","西番雅书","哈该书","撒迦利亚书","玛拉基书","新约全书 - N.T.","马太福音","马可福音","路加福音","约翰福音","使徒行传","罗马书","哥林多前书","哥林多后书","加拉太书","以弗所书","腓立比书","歌罗西书","帖撒罗尼迦前书","帖撒罗尼迦后书","提摩太前书","提摩太后书","提多书","腓利门书","希伯来书","雅各书","彼得前书","彼得后书","约翰一书","约翰二书","约翰三书","犹大书","启示录"],
+    ["创世记","出埃及记","利未记","民数记","申命记","约书亚记","士师记","路得记","撒母耳记上","撒母耳记下","列王纪上","列王纪下","历代志上","历代志下","以斯拉记","尼希米记","以斯帖记","约伯记","诗篇","箴言","传道书","雅歌","以赛亚书","耶利米书","耶利米哀歌","以西结书","但以理书","何西阿书","约珥书","阿摩司书","俄巴底亚书","约拿书","弥迦书","那鸿书","哈巴谷书","西番雅书","哈该书","撒迦利亚书","玛拉基书","新约全书 - N.T.","马太福音","马可福音","路加福音","约翰福音","使徒行传","罗马书","哥林多前书","哥林多后书","加拉太书","以弗所书","腓立比书","歌罗西书","帖撒罗尼迦前书","帖撒罗尼迦后书","提摩太前书","提摩太后书","提多书","腓利门书","希伯来书","雅各书","彼得前书","彼得后书","约翰一书","约翰二书","约翰三书","犹大书","启示录"],
+);
+bible.addNames(
+    "zh-TW",
+    bible.DEFAULT_BIBLE,
+    ["創 世 記","出 埃 及 記","利 未 記","民 數 記","申 命 記","約 書 亞 記","士 師 記","路 得 記","撒 母 耳 記 上","撒 母 耳 記 下","列 王 紀 上","列 王 紀 下","歷 代 志 上","歷 代 志 下","以 斯 拉 記","尼 希 米 記","以 斯 帖 記","約 伯 記","詩 篇","箴 言","傳 道 書","雅 歌","以 賽 亞 書","耶 利 米 書","耶 利 米 哀 歌","以 西 結 書","但 以 理 書","何 西 阿 書","約 珥 書","阿 摩 司 書","俄 巴 底 亞 書","約 拿 書","彌 迦 書","那 鴻 書","哈 巴 谷 書","西 番 雅 書","哈 該 書","撒 迦 利 亞","瑪 拉 基 書","馬 太 福 音","馬 可 福 音","路 加 福 音","約 翰 福 音","使 徒 行 傳","羅 馬 書","歌 林 多 前 書","歌 林 多 後 書","加 拉 太 書","以 弗 所 書","腓 立 比 書","歌 羅 西 書","帖 撒 羅 尼 迦 前 書","帖 撒 羅 尼 迦 後 書","提 摩 太 前 書","提 摩 太 後 書","提 多 書","腓 利 門 書","希 伯 來 書","雅 各 書","彼 得 前 書","彼 得 後 書","約 翰 一 書","約 翰 二 書","約 翰 三 書","猶 大 書","启 示 录"],
+    ["創 世 記","出 埃 及 記","利 未 記","民 數 記","申 命 記","約 書 亞 記","士 師 記","路 得 記","撒 母 耳 記 上","撒 母 耳 記 下","列 王 紀 上","列 王 紀 下","歷 代 志 上","歷 代 志 下","以 斯 拉 記","尼 希 米 記","以 斯 帖 記","約 伯 記","詩 篇","箴 言","傳 道 書","雅 歌","以 賽 亞 書","耶 利 米 書","耶 利 米 哀 歌","以 西 結 書","但 以 理 書","何 西 阿 書","約 珥 書","阿 摩 司 書","俄 巴 底 亞 書","約 拿 書","彌 迦 書","那 鴻 書","哈 巴 谷 書","西 番 雅 書","哈 該 書","撒 迦 利 亞","瑪 拉 基 書","馬 太 福 音","馬 可 福 音","路 加 福 音","約 翰 福 音","使 徒 行 傳","羅 馬 書","歌 林 多 前 書","歌 林 多 後 書","加 拉 太 書","以 弗 所 書","腓 立 比 書","歌 羅 西 書","帖 撒 羅 尼 迦 前 書","帖 撒 羅 尼 迦 後 書","提 摩 太 前 書","提 摩 太 後 書","提 多 書","腓 利 門 書","希 伯 來 書","雅 各 書","彼 得 前 書","彼 得 後 書","約 翰 一 書","約 翰 二 書","約 翰 三 書","猶 大 書","启 示 录"],
+);
